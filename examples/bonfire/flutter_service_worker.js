65084a643b833fba8b4f3ee3952f746175623c83
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2021-08-31 11:50:25 -0300

    atualiza exemplo

--- examples/bonfire/flutter_service_worker.js
+++ examples/bonfire/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "c566c957085bad5a40b535027107856e",
-"/": "c566c957085bad5a40b535027107856e",
-"main.dart.js": "8792449e79066d63349526434bfd6ebe",
+"index.html": "7bbb9c5be619c3a60c4e413e0974bd1c",
+"/": "7bbb9c5be619c3a60c4e413e0974bd1c",
+"main.dart.js": "b5f0916acbd62540ee20d5da49d323ef",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
