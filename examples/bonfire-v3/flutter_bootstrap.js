4e1d7bb1b24a74dfcd4bb4732c7e21d906e8f4d7
Rafael <rafaelbarbosatec@gmail.com>
2025-06-02 07:51:16 -0300

    chore: update example

--- examples/bonfire-v3/flutter_bootstrap.js
+++ examples/bonfire-v3/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "85061887"
+    serviceWorkerVersion: "2690216105"
   }
 });
